86537aaee5aa4313e5d8e7a1679458be3ea7727d
nicollialura <117859906+nicollialura@users.noreply.github.com>
2024-04-05 08:20:47 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -17,3 +17,15 @@ function calculaTempo(tempoObjetivo) {
     let segundos = Math.floor (tempoFinal / 1000);
     return segundos;
   }
+const tempoObjetivo2 = new Date("2023-12-05T00:00:00");
+const tempoObjetivo3 = new Date("2023-12-30T00:00:00");
+const tempoObjetivo4 = new Date("2024-02-01T00:00:00");
+
+for (let i=0; i<contadores.length;i++)contadores[1].textContent = calculaTempo(tempos[i]);
+contadores[2].textContent = calculaTempo(tempos[2]);
+contadores[3].textContent = calculaTempo(tempos[3]);
+contadores[4].textContent = calculaTempo(tempos[4]);
+const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
+contadores[0].textContent = calculaTempo(tempos[0]); 
+for (let i=0; i<contadores.length;i++){contadores[i].textContent = calculaTempo(tempos[i]); }
+              
